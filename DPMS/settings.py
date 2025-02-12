1225bbba449d358ddfe523f356981549c8f34832
Bulemu Shilukukwa <narcoa47@gmail.com>
2025-02-12 14:31:04 +0200

    allowed_hosts

--- DPMS/settings.py
+++ DPMS/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-wc*-&gs&if#m&_@y49q6*4fx-9z&ej_9@k5x_=_=iq@vxak)f%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "4.236.160.125:8000"]
 
 # Application definition
 INSTALLED_APPS = [
